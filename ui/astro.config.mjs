5cc4a6c6fdf422ff5acfd69a37a91ed26b7bde72
robin <robin@rnode.me>
2022-10-20 17:44:38 +0800

    chore: add tailwind css

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  integrations: [tailwind()],
   adapter: node({
     mode: "standalone",
   }),
