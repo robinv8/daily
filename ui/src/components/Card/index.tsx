7e255fefe53a9d50feb5c0931e548b26aff07c8a
Ren Yubin <robin@sifou.com>
2022-12-03 00:36:16 +0800

    feat: 封面支持视频

--- ui/src/components/Card/index.tsx
+++ ui/src/components/Card/index.tsx
@@ -1,16 +1,30 @@
 const Card = ({ items }) => {
+  const isVideo = items.image_url.includes('.mp4');
   return (
     <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm">
       <a href={items.origin_url} target="_blank">
-        <img
-          src={items.image_url}
-          loading="lazy"
-          alt={items.title}
-          className="w-full h-48 rounded-t-md object-contain"
-        />
+        {isVideo ? (
+          <video
+            className="w-full h-48 object-cover rounded-t-md"
+            src={items.image_url}
+            autoPlay
+            loop
+            muted
+          />
+        ) : (
+          <img
+            src={items.image_url}
+            loading="lazy"
+            alt={items.title}
+            className="w-full h-48 rounded-t-md object-contain"
+          />
+        )}
+
         <div className="pt-3 ml-4 mr-2 mb-3">
           <h3 className="text-xl text-gray-900">{items.title}</h3>
-          <p className="text-gray-400 text-sm mt-1 line-clamp-3">{items.description}</p>
+          <p className="text-gray-400 text-sm mt-1 line-clamp-3">
+            {items.description}
+          </p>
         </div>
       </a>
     </article>
